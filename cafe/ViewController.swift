443d90585b2fcacea52e0a0aaefe752ed387ca1f
Arbe <arbe0102@gmail.com>
2020-12-25 10:09:19 +0800

    get api final

--- cafe/ViewController.swift
+++ cafe/ViewController.swift
@@ -1,19 +1,49 @@
-//
-//  ViewController.swift
-//  cafe
-//
-//  Created by arbe on 2020/12/17.
-//
-
 import UIKit
+import Foundation
+
+struct Cafe: Decodable {
+    var id: String?
+    var name: String?
+    var city: String?
+    var wifi: Float?
+    var seat: Float?
+    var quiet: Float?
+    var tasty: Float?
+    var cheap: Float?
+    var music: Float?
+    var url: String?
+    var address: String?
+    var latitude: String?
+    var longitude: String?
+    var limited_time: String?
+    var socket: String?
+    var standing_desk: String?
+    var mrt: String?
+    var open_time: String?
+}
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let urlStr = "https://cafenomad.tw/api/v1.2/cafes"
+        if let url = URL(string: urlStr) {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            let task = URLSession.shared.dataTask(with: url) { (data, responce, error) in
+                if let data = data {
+                    do {
+                        let searchResult = try decoder.decode([Cafe].self, from: data)
+                        print(searchResult[0].name!)
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
     }
-
-
 }
-
